12a2b2a6fe59586bf1653b846a9fb58b70857489
Josseluu <jslslpz@myuax.com>
2023-11-30 09:06:04 +0100

    Initial commit

--- Vector e interadores.cpp
+++ Vector e interadores.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+
+    std::vector<int> miVector;
+
+
+    miVector.push_back(10);
+    miVector.push_back(20);
+    miVector.push_back(30);
+    miVector.push_back(40);
+
+    std::cout << "Elementos del vector: ";
+    for (std::vector<int>::iterator it = miVector.begin(); it != miVector.end(); ++it) {
+        std::cout << *it << " ";
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
 
